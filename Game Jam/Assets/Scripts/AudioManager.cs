86620f6410c84cf0853debd41c1094ef121537ca
Alexcool3 <Alex33@live.dk>
2020-01-27 23:52:26 +0100

    AudioManager Setup

---
+++ Game Jam/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region AudioManager instance
+    private static AudioManager instance;
+    public static AudioManager audioManager
+    {
+        get
+        {
+            return instance;
+        }
+
+        private set
+        {
+            if (instance != null)
+            {
+                Destroy(value.gameObject);
+                return;
+            }
+            instance = value;
+        }
+    }
+    #endregion
+
+    #region AudioSources
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource musicSource2;
+    [SerializeField] private AudioSource sfxSource;
+    #endregion
+
+    private bool firstMusicSourceIsPlaying;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void PlayMusic(AudioClip musicClip)
+    {
+        // Determine which sourcer is active
+        AudioSource activeSource = (firstMusicSourceIsPlaying) ? musicSource : musicSource2;
+
+        musicSource.clip = musicClip;
+        activeSource.volume = 1;
+        musicSource.Play();
+
+    }
+
+    public void PlayMusicWithFade(AudioClip newClip, float transitionTime = 1.0f)
+    {
+        // Determine which source is active
+        AudioSource activeSource = (firstMusicSourceIsPlaying) ? musicSource : musicSource2;
+        StartCoroutine(UpdateMusicWithFade(activeSource, newClip, transitionTime));
+    }
+
+    public void PlayMusicWithCrossFade(AudioClip musicClip, float transitionTime = 1.0f)
+    {
+        // Determine which source is active
+        AudioSource activeSource = (firstMusicSourceIsPlaying) ? musicSource : musicSource2;
+        AudioSource newSource = (firstMusicSourceIsPlaying) ? musicSource2 : musicSource;
+
+        // Swap the source 
+        firstMusicSourceIsPlaying = !firstMusicSourceIsPlaying;
+
+        // Set the fields of the audio source, then start the coroutine to crossfade
+        newSource.clip = musicClip;
+        newSource.Play();
+        StartCoroutine(UpdateMusicWithCrossFade(activeSource, newSource, transitionTime));
+    }
+
+    #region PlaySFX Methods
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PlaySFX(AudioClip clip, float volume)
+    {
+        sfxSource.PlayOneShot(clip, volume);
+    }
+    #endregion
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        musicSource2.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+
+    // Method: fades out the current audio clip and fades in a new. 
+    private IEnumerator UpdateMusicWithFade(AudioSource acitveSource, AudioClip newClip, float transitionTime)
+    {
+        // Make sure the source is active and playing 
+        if (!acitveSource.isPlaying)
+        {
+            acitveSource.Play();
+        }
+
+        // Fade Out
+        for (float t = 0; t < transitionTime; t += Time.deltaTime)
+        {
+            acitveSource.volume = (1 - (t / Time.deltaTime)); // Lower the volume of the active source for each second.
+            yield return null;
+        }
+
+        // Stop the current clip, assign the current clip to the new clip and play it. 
+        acitveSource.Stop();
+        acitveSource.clip = newClip;
+        acitveSource.Play();
+
+        // Fade In
+        for (float t = 0; t < transitionTime; t += Time.deltaTime)
+        {
+            acitveSource.volume = (t / Time.deltaTime); // increase the volume of the active source for each second.
+            yield return null;
+        }
+    }
+
+    // Fades out current source and fades in new source simultaneously.
+    private IEnumerator UpdateMusicWithCrossFade(AudioSource original, AudioSource newSource, float transitionTime)
+    {
+
+        for (float t = 0.0f; t < transitionTime; t += Time.deltaTime)
+        {
+            original.volume = (1 - (t / Time.deltaTime)); // Fades out the current source by lowering the volume.
+            newSource.volume = (t / Time.deltaTime); // Fades in the new source by increasing the volume.
+            yield return null;
+        }
+
+        original.Stop();
+
+    }
+
+
+
+}
--- Game Jam/Assets/Scripts/GameManager.cs
+++ Game Jam/Assets/Scripts/GameManager.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    #region GameManager instance
+    private static GameManager instane;
+
+    public static GameManager gameManager
+    {
+        get
+        {
+            return instane;
+        }
+
+        set
+        {
+            if (instane != null)
+            {
+                instane = value;
+            }
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        gameManager = this;
+    }
+
+
+
 }
