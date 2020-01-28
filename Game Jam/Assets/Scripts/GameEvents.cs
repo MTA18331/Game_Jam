c6b60bc9293e8c3e2a647f19231af9ce7f54c7da
Alexcool3 <Alex33@live.dk>
2020-01-28 21:00:11 +0100

    ChurchEventSound added

---
+++ Game Jam/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    #region GameEvents Instance 
+    public static GameEvents gameEvents
+    {
+        get
+        {
+            return instance;
+        }
+
+        set
+        {
+            if (instance != null)
+            {
+                Destroy(value.gameObject);
+                return;
+            }
+
+            instance = value;
+        }
+    }
+    private static GameEvents instance = null; // Singleton Instance
+    #endregion
+
+    public event Action<int> onChurchCollide;
+
+    private void Awake()
+    {
+        gameEvents = this;
+    }
+
+    public void ChurchCollide(int id)
+    {
+        onChurchCollide?.Invoke(id);
+    }
+
+
+}
